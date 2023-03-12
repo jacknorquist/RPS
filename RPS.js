3f760dc81be7906d359c7487f483f821fb9ad708
Jack Norquist <jack.norquist@outlook.com>
2023-03-11 20:42:16 -0600

    addding rps ui

--- RPS.js
+++ RPS.js
@@ -1,6 +1,6 @@
 let playerScore = 0;
 function resetPlayerScore(){
-    score = 0;
+    playerScore = 0;
 }
 let compScore = 0;
 function resetCompScore(){
@@ -9,70 +9,97 @@ function resetCompScore(){
 
 function getComputerSelection() {
     const options = ["Rock", "Paper", "Scissors"];
-    const computerSelection = options[Math.floor(Math.random() * options.length)];
-    console.log(computerSelection);
-    return computerSelection;
+    comp = options[Math.floor(Math.random() * options.length)];
 }
 
-
-function getPlayerSelection() {
-    const playerSelection = document.getElementById("myInput").value;
-    console.log(playerSelection);
-    return playerSelection;
-}
-
+let comp = "";
+let playerSelection="";
 
 function playRound(){
-    const comp = getComputerSelection();
-    const player = getPlayerSelection();
 
-
-    if (comp === "Paper" && player === "Scissors" || comp === "Rock" && player === "Paper" || comp === "Scissors" && player === "Rock"){
-        console.log("You Win The Round");
+    if (comp === "Paper" && playerSelection === "Scissors" || comp === "Rock" && playerSelection === "Paper" || comp === "Scissors" && playerSelection === "Rock"){
+        alert("You Win The Round");
         playerScore++;
-        console.log(playerScore);
-        console.log(compScore);
-    }
-    else if (comp === "Rock" && player === "Scissors" || comp === "Paper" && player === "Scissors" || comp === "Scissors" && player === "Paper" || comp === "Paper" && player === "Rock"){
-        console.log("You Lose The Round");
+        console.log(playerSelection);
+    }    
+    else if (comp === "Rock" && playerSelection === "Scissors" || comp === "Paper" && playerSelection === "Scissors" || comp === "Scissors" && playerSelection === "Paper" || comp === "Paper" && playerSelection === "Rock"){
+        alert("You Lose The Round");
         compScore++;
-        console.log(playerScore);
-        console.log(compScore);
+        console.log(playerSelection);
+        
+      
     }
-    else if (comp === "Paper" && player === "Paper" || comp === "Rock" && player === "Rock" || comp === "Scissors" && player === "Scissors"){
-        console.log("Tie");
-        console.log(playerScore);
-        console.log(compScore);
-    }
+    else if (comp === "Paper" && playerSelection === "Paper" || comp === "Rock" && playerSelection === "Rock" || comp === "Scissors" && playerSelection === "Scissors"){
+        alert("Tie");
+    }    
     else{
         console.log("Play the game Dummy")
     }
 }
-let roundsPlayed = 0;
+let roundsPlayed = 1;
 function getRoundsPlayed(){
     roundsPlayed++;
-    console.log(roundsPlayed);
 }
 function resetRoundsPlayed(){
-    roundsPlayed = 0;
+    roundsPlayed = 1;
 }
+
+
+
+const rockB = document.querySelector('#rockb')
+const paperB = document.querySelector('#paperb')
+const scissorsB = document.querySelector('#scissorsb')
+
+function rock(){
+    playerSelection = "Rock";
+    console.log(playerSelection);
+    getComputerSelection();
+    playRound();
+    game();
+}
+
+rockB.addEventListener('click', rock)
+
+function paper(){
+    playerSelection = "Paper";
+    getComputerSelection();
+    playRound();
+    game();
+   
+}
+paperB.addEventListener('click', paper)
+
+function scissors(){
+    playerSelection = "Scissors";
+    getComputerSelection();
+    playRound();
+    game();
+}
+scissorsB.addEventListener('click', scissors)
+
+
 function game () {
     if (roundsPlayed <= 4 ){
-        playRound();
         getRoundsPlayed();
     } else if (roundsPlayed == 5){
         if(playerScore > compScore) {
-            console.log("You Win The Game")
+            alert("You Win The Game")
+            alert(playerScore);
+            alert("to");
+            alert(compScore);
             resetRoundsPlayed();
             resetPlayerScore ();
-            resetCompScore;
+            resetCompScore();
         } else if (playerScore < compScore){
-            console.log("You Lose The Game")
+            alert("You Lose The Game")
+            alert(compScore);
+            alert("to");
+            alert(playerScore);
             resetRoundsPlayed();
             resetPlayerScore(); 
             resetCompScore();       
         } else if (playerScore == compScore){
-            console.log("Tie Game!")
+            alert("Tie Game!");
             resetRoundsPlayed();
             resetPlayerScore();
             resetCompScore();
